346eedf8f27bee3c10018d45e7692c44287a5e9d
MarioSantoro <mariosantoro124@gmail.com>
2023-07-20 15:17:59 +0200

    Complete the project with bonus

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,5 +2,23 @@
 
 
 @section('main-content')
-    <h1>Benvenuti nel Main</h1>
+    <div class="container">
+        <div class="row">
+            @foreach ($movies as $movie)
+                <div class="col-3">
+                    <div class="card" style="width: 18rem;">
+                        <div class="card-body">
+                            <h1>Film : {{ $movie->id }}</h1>
+                            <h5 class="card-title">Titolo : {{ $movie->title }}</h5>
+                            <p class="card-text">Titolo Originale : {{ $movie->original_title }}</p>
+                            <p class="card-text">Nazionalità : {{ $movie->nationality }}</p>
+                            <p class="card-text">Data usicta : {{ $movie->date }}</p>
+                            <p class="card-text">Voto :{{ $movie->vote }}</p>
+                            <a href="#" class="btn btn-primary">Guarda</a>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
